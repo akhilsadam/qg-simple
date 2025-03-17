a002fd7a95d1a28e07519d4a52a8f57643aa2ff4
Akhil Sadam <akhilsadam.mit@gmail.com>
2025-03-17 19:40:47 -0400

    update install script

--- src/install/install.sh
+++ src/install/install.sh
@@ -3,8 +3,7 @@ conda create --prefix .sw python=3.10.15 git tmux graphviz conda-forge::ffmpeg
 conda activate .sw
 python3 -m pip install uv
 uv venv
-source .venv/bin/activate
-python3 -m uv pip install -r requirements.txt
+source .venv/bin/activate && uv pip install -r src/install/requirements.txt
 
 # don't forget to set paths!
 # USER_CONDA_ENV_PATH=/projects/ml_scope/Students/a1744874/scale_invariants/sw
