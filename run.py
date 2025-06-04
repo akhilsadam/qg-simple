590977d4c6015d7ffb82207ab0fc0b7ed096c613
Akhil Sadam <akhilsadam.mit@gmail.com>
2025-06-04 14:00:33 -0400

    fix input

--- run.py
+++ run.py
@@ -1,13 +1,19 @@
-if __name__ == "__main__":
+import sys
+
+def run(args):
     
-    import sys
     from mura.deploy.run import Run
     from mura.deploy.util import cprint
     from qg.solver.qg import QG
     
-    with Run(sys.argv) as info:
-        param, wandb_logger, py_logger, version, save_path = info
+    with Run(args) as info:
+        config, wandb_logger, py_logger, version, save_path = info
+        
         py_logger.info(version)
         py_logger.info(save_path)
-        py_logger.info(cprint(param))
-        QG(param,logger=py_logger).solve(save_path=save_path)
+        py_logger.info(cprint(config))
+        
+        QG(config,logger=py_logger).solve(save_path=save_path)
+
+if __name__ == "__main__":
+    run(sys.argv)
