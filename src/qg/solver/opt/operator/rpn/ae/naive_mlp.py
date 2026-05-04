50c4b59a630ea1b65d35bb0d20712d1d78459b7b
Akhil Sadam <sadam.akhil@gmail.com>
2026-05-04 19:37:12 -0400

    fix tokenizer for LLM

--- src/qg/solver/opt/operator/rpn/ae/naive_mlp.py
+++ src/qg/solver/opt/operator/rpn/ae/naive_mlp.py
@@ -68,19 +68,19 @@ class RPN_AE(nn.Module):
         self.embedder = embedder
         
         self.proj = nn.Sequential(
+            MixerBlock(seq_len, embed_dim, seq_len * 4, proj_dim),
+            MixerBlock(seq_len, embed_dim, seq_len * 4, proj_dim),
             nn.Flatten(-2,-1),
-            MixerBlock(seq_len, embed_dim, seq_len * 4, proj_dim),
-            MixerBlock(seq_len, embed_dim, seq_len * 4, proj_dim),
             nn.Linear(seq_len * embed_dim, proj_dim),
         )
         
         token_dim = proj_dim + embed_dim
         
         self.unproj = nn.Sequential(
+            MixerBlock(seq_len, token_dim, seq_len * 4, token_dim * 2),
+            MixerBlock(seq_len, token_dim, seq_len * 4, token_dim * 2),
             nn.Flatten(-2,-1),
-            MixerBlock(seq_len, token_dim, seq_len * 4, token_dim * 2),
-            MixerBlock(seq_len, token_dim, seq_len * 4, token_dim * 2),
-            nn.Linear(seq_len * token_dim, seq_len * embed_dim),
+            nn.Linear(seq_len * todim, seq_len * embed_dim),
             nn.Unflatten(-1, (seq_len, embed_dim)),
         )      
         
