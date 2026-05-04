f2506259937e6cd0dfcd13097eb1022b44beeff3
Akhil Sadam <sadam.akhil@gmail.com>
2026-05-04 02:16:47 -0400

    fix tokenizer

--- src/qg/solver/opt/operator/rpn/ae/att2.py
+++ src/qg/solver/opt/operator/rpn/ae/att2.py
@@ -103,7 +103,7 @@ class RPN_AE(nn.Module):
         self.pad_token_id = TOKEN_TO_ID["__pad__"]
         
     def zero(self):
-        id_ = torch.full((1, 1), self.pad_token_id, dtype=torch.long, device=self.pe_fwd.device)
+        id_ = torch.full((1, 1), self.pad_token_id, dtype=torch.long, device=self.freqs.device)
         amp_ = torch.ones(1, 1, dtype=torch.float32, device=id_.device)
         pad = self.embedder(id_, amp_)
         return pad
